5550a092457af36cd8e16e2e5518b4131f6b8842
Paul.py <pauliukaegbu@gmail.com>
2020-08-01 02:27:22 -0500

    changes

--- studentpairing.py
+++ studentpairing.py
@@ -1,4 +1,4 @@
-# This is a Python Dictionary that contains all of the students in Kenny's class as well as their grades.
+# This is a Python Dictionary that contains all of the students in a class as well as their grades.
 student_grades = {"Jeremy" : 87, "Kyla" : 82, "Ayesha" : 90, "Aleida" : 94, "Todd" : 79, "Maxwell" : 98, "Yolonda" : 81, "Kiyoko" : 71, "Dagmar" : 73, "Laura" : 91, "Shimeah" : 81, "Songqiao" : 92, "Frankie" : 87, "Natalia" : 95, "Gonzalo" : 82, "Pavel" : 78}
 
 # This is a function that determines the student with the highest grade given a dictionary
@@ -23,7 +23,7 @@ def lowest_grade(grades):
 				bottom_of_class = student_b
 	return bottom_of_class
 
-# This is Kenny's Algorithm! It sorts the students into pairs based on their grades.
+# This is the Algorithm! It sorts the students into pairs based on their grades.
 def kennys_algorithm(grade_dict):
 	student_pairs = []
 	while len(grade_dict) > 0:
